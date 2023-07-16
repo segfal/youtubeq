c2afcd2a1cf05f5749ee41461a3d15f9077c3049
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-07-16 14:55:52 -0400

    setting everything up

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+const cors = require('cors');
+cors(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+
+
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+
+
+
+
+
